18aac492596d30d3b421c8751adcc7e1a8f92ae3
Axel Rindle <mail@axelrindle.de>
2023-06-06 14:42:46 +0200

    build: split vendor chunk

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+  ],
   build: {
     outDir: '../src/main/resources/static',
     emptyOutDir: true,
-  }
+  },
 })
