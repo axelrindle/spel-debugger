212aeaf507c86b767897cddcb988b9dba78e2366
Axel Rindle <mail@axelrindle.de>
2023-09-16 23:26:32 +0200

    refactor: always return an URL instance

--- frontend/src/api/url.ts
+++ frontend/src/api/url.ts
@@ -1,6 +1,6 @@
-export default function getUrl(relative: string): string|URL {
+export default function getUrl(relative: string): URL {
     if (import.meta.env.PROD) {
-        return '/' + relative;
+        return new URL('/' + relative);
     }
 
     return new URL('/' + relative, import.meta.env.VITE_API_URL)
